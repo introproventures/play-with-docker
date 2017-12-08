a4805045b879bac7e8795bc0398ea2dab9c62d71
Alex Merkulov <almerico@gmail.com>
2017-12-08 21:33:16 +0200

    Update gitcheck.sh

--- gitcheck.sh
+++ gitcheck.sh
@@ -7,8 +7,17 @@ do
 	git status -uno |grep "Your branch is up-to-date with 'origin/master'" >/dev/null
 	if [ $? != 0 ]
 	then
-	  echo `date` "Changes detected."
-	  docker-compose up
+	  echo `date` " Changes detected. Stopping previous run. "
+	  docker-compose down &
+	  
+          sleep 5
+	  echo `date` " Cleaning system"
+          docker system prune -f
+	  echo `date` " Pull new changes"
+          git pull
+	  echo `date` " Start new version"
+          docker-compose up &
+	  
 	fi
 	sleep 1m
 done
